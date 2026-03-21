c9c9eb31b91b1b8a877e43e101acbc159bf6e35a
Stuart Todd <hello@halfshellstudios.co.uk>
2026-03-21 20:58:48 +0000

    exercise 25 - docker set up

--- 25-docker-setup/target/debug/deps/docker_setup-4f1b3f43f4c95964.d
+++
@@ -1,5 +0,0 @@
-/Users/stuart/PhpstormProjects/php-to-rust/25-docker-setup/target/debug/deps/docker_setup-4f1b3f43f4c95964.d: src/main.rs
-
-/Users/stuart/PhpstormProjects/php-to-rust/25-docker-setup/target/debug/deps/docker_setup-4f1b3f43f4c95964: src/main.rs
-
-src/main.rs:
--- 25-docker-setup/target/debug/docker-setup.d
+++
@@ -1,1 +0,0 @@
-/Users/stuart/PhpstormProjects/php-to-rust/25-docker-setup/target/debug/docker-setup: /Users/stuart/PhpstormProjects/php-to-rust/25-docker-setup/src/main.rs
